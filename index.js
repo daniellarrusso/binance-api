980214d56ee016d8db3cfb52fbac76e2d3532ac2
Nathan Adams <nathanadams123@gmail.com>
2020-02-07 23:35:42 +0000

    express added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const binance = require('./credentials');
+const express = require('express');
 
-binance('RVNBTC', data => {
-  console.log(data);
+const app = express();
+
+app.get('api/prices', (req, res) => {
+  binance('RVNBTC', data => {
+    res.send(res);
+  });
 });
+
+app.listen(3000, () => console.log('Listening on port 3000'));
