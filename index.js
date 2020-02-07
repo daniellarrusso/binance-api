54bb5be73ee19afaa19d3d4cf3417fa504f805e2
Nathan Adams <nathanadams123@gmail.com>
2020-02-07 23:53:20 +0000

    fixed port

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.get('/api/prices', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
