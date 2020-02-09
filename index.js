4ff8ccec63d108ed09c6522aeb74d7fa89cbeae8
Nathan Adams <nathanadams123@gmail.com>
2020-02-09 00:39:38 +0000

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/api/prices', (req, res) => {
 });
 
 app.post('/api/buy', (req, res) => {
-  return { message: 'bought something' };
+  res.send(req.body);
 });
 
 app.get('/api/pairings', (req, res) => {
